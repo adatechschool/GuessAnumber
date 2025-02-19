0500cf1b70858fd2282f49764ac8ad20d68baa31
Flaya <delphine.jozeau@gmail.com>
2025-02-19 21:38:15 +0100

    version fonctionnelle

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function gessedNumber() {
   let givenNumber1 = parseInt(prompt("Donne-moi un nombre entre 0 et 50"))
-  while(givenNumber1 > 50 || givenNumber1 < 0){
+  while (givenNumber1 > 50 || givenNumber1 < 0) {
     givenNumber1 = prompt("entre 0 et 50 !")
   }
   return givenNumber1
 }
-//entrer le nombre 1
+//entrer le nombre1
 
 function recupNumber2() {
   let givenNumber2 = document.getElementById("givenNumber2").value
@@ -15,47 +15,43 @@ function recupNumber2() {
 
 function didWin(givenNumber1, givenNumber2) {
   if (givenNumber2 == givenNumber1) {
-    alert("Bravo! vous avez deviné le nombre.");
+    let game = document.getElementById("game");
+    game.style.display ="none";
+    let win = document.getElementById("win");
+    win.style.display = "inline-block"
     return true
   }
-  else if (givenNumber2 < givenNumber1) { 
-     alert("Plus grand"); 
-    }
+  else if (givenNumber2 < givenNumber1) {
+    alert("Plus grand");
+    let min = document.getElementById("min");
+    min.innerText = givenNumber2;
+  }
   else {
-    alert("Plus petit"); 
-    }
+    alert("Plus petit");
+    let max = document.getElementById("max");
+    max.innerText = givenNumber2;
+  }
   return false
   //retourne le résultat mais pas le nom
 }
 
-function tentatives() {
-
-}
-
-function Effassoor(){
-
-}  
-
-
 function gamePlay() {
+  const clickButton = document.querySelector('#validate')
+  count++; clickButton.innerText = `Nombre de tentatives : ${count}`;
   let givenNumber2 = recupNumber2();
-  let Win = didWin(givenNumber1,givenNumber2)
-  if(Win){
-    Effassoor()
-  }
- /* while (didWin(givenNumber1,givenNumber2) === false) {
-    ;
-    } */
-  //la fonction se rappelle elle même si False
+  let Win = didWin(givenNumber1, givenNumber2)
+
 }
-// gamePlay gere les appels de fonctions et la transmission de votre variable 
+// gamePlay gere les appels de fonctions et la transmission de la variable 
 // d’une fonction à une autre.
+
 //Dans la fonction gamePlay, si didIWin a retourné true, on arrete le jeu. 
 
-let givenNumber1 = gessedNumber()
+let givenNumber1 = gessedNumber();
 //je stocke le résultat de la fonction
-let validate = document.getElementById("validate")
-validate.addEventListener('click', gamePlay)
+let validate = document.getElementById("validate");
+let count = 0;
+validate.addEventListener('click', gamePlay);
 
 
 
